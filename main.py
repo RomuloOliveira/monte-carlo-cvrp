0dd9565989880cd73891da0fb78c92e97265239e
Rômulo Oliveira <romulo.ac.oliveira@gmail.com>
2014-10-10 11:54:25 -0300

    structuring files

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+
+from project import input
+
+def usage():
+    print "python {} <tspblib_file>".format(sys.argv[0])
+
+def main():
+    if len(sys.argv) != 2: # python main.py <file>
+        return usage()
+
+    data = input.read_data(sys.argv[1])
+
+if __name__ == '__main__':
+    main()
--- project/input.py
+++ project/input.py
@@ -1,2 +1,6 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
+
+def read_data(filename):
+    """Reads a TSPLIB file and returns the problem data"""
+    raise NotImplementedError()
