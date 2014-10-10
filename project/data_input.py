469740e46106a38496988c25dd533d3a1e11a4c6
Rômulo Oliveira <romulo.ac.oliveira@gmail.com>
2014-10-10 16:31:08 -0300

    initial input reader

--- project/data_input.py
+++ project/data_input.py
@@ -8,8 +8,35 @@ def sanitize(filename):
 
     Example: ~/input.txt -> /home/<your_home/input.txt
     """
-    os
+    # TODO: sanitize
+    # See:
+    return filename
 
 def read_file(filename):
     """Reads a TSPLIB file and returns the problem data"""
-    raise NotImplementedError()
+    sanitized_filename = sanitize(filename)
+
+    line = ''
+
+    specs = {}
+
+    used_specs = ['NAME', 'COMMENT', 'DIMENSION', 'CAPACITY', 'TYPE']
+    used_data = ['NODE_COORD_SECTION', 'DEMAND_SECTION', 'DEPOT_SECTION']
+
+    # Parse specs part
+    for line in open(sanitized_filename):
+        line = line.replace('\r\n', '').strip() # remove new lines and trailing whitespaces
+        for s in used_specs:
+            if line.startswith(s):
+                print s, line
+                specs[s] = line.split('{} :'.format(s))[-1].strip() # get value data part
+                break
+
+        # All specs read
+        if len(specs) == len(used_specs):
+            break
+
+    if len(specs) != len(used_specs):
+        raise Exception('Error parsing TSPLIB data: {} missing'.format(set(used_specs) - set(specs)))
+
+    print specs
