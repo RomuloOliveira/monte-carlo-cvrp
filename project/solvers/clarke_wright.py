3eca34ee8518194ad0aad3f164038bf24bcdb928
Rômulo Oliveira <romulo.ac.oliveira@gmail.com>
2014-10-19 21:53:45 -0200

    cleaner code

--- project/solvers/clarke_wright.py
+++ project/solvers/clarke_wright.py
@@ -26,27 +26,8 @@ def add_node(data, allocation, i, vehicle, vehicles_run, vehicles_demand, append
     if i in allocation:
         old_vehicle = allocation[i]
 
-        print 'i', i
-        print 'old_vehicle', old_vehicle
-        print 'vehicles_demand', vehicles_demand
-
-        if old_vehicle < len(vehicles_demand):
-            vehicles_demand[old_vehicle] = vehicles_demand[old_vehicle] - demand
-        else:
-            print 'como nao?'
-
-        print 'vehicles_demand depois', vehicles_demand
-
-        if i in vehicles_run[old_vehicle]:
-            print 'ta aqui'
-            vehicles_run[old_vehicle].remove(i)
-        else:
-            print '{} not in vehicles_run'.format(old_vehicle)
-
-        # # Remove 0-nodes routes
-        # if not vehicles_run[old_vehicle]:
-        #     del vehicles_demand[old_vehicle]
-        #     del vehicles_run[old_vehicle]
+        vehicles_demand[old_vehicle] = vehicles_demand[old_vehicle] - demand
+        vehicles_run[old_vehicle].remove(i)
 
     vehicles_demand[vehicle] = vehicles_demand[vehicle] + demand
     allocation[i] = vehicle
@@ -129,155 +110,49 @@ def compute_savings_list(data):
 
     sorted_savings_list = sorted(savings_list.items(), key=operator.itemgetter(1), reverse=True)
 
-    print 'sorted_savings_list', sorted_savings_list
-
     return [nodes for nodes, saving in sorted_savings_list]
 
 def solve(data, vehicles):
     """Solves the CVRP problem using Clarke and Wright Savings methods"""
     savings_list = compute_savings_list(data)
 
-    vehicles_run = {}
-    vehicles_demand = {}
-
-    print vehicles_run
-    print vehicles_demand
-
-    depot = data['DEPOT']
-
-    # for i, j in savings_list:
-    #     #
-    #     # (I)   t(0,i) and t(0, j) must be greater than zero.
-    #     # (II)  p(i) and p(y) are not already allocated on the same truck run.
-    #     # (III) Removing the trucks allocated to Q(i) and Q(j) and adding a truck to cover the load Q(i)+Q(j)
-    #     #       does not cause the trucks allocated to exceed the trucks available
-    #     #
-    #     if routes[depot][i] > 0 and routes[depot][j] > 0:
-    #         if allocations[i] != allocations[j]:
-    #             vehicle_a = allocations[i]
-    #             vehicle_b = allocations[j]
-
-    #             if can_add(data, [j], vehicles_demand[vehicle_a]):
-    #                 add_node(data, allocations, j, vehicle_a, vehicles_run, vehicles_demand)
-    #                 routes[i][j] = 1
-    #                 routes[depot][i] = 1
-    #                 routes[depot][j] = 1
-    #             elif can_add(data, [i], vehicles_demand[vehicle_b]):
-    #                 add_node(data, allocations, i, vehicle_b, vehicles_run, vehicles_demand)
-    #                 routes[i][j] = 1
-    #                 routes[depot][i] = 1
-    #                 routes[depot][j] = 1
-
     vehicles_run = [[] for _ in range(vehicles)]
     vehicles_demand = [0 for _ in range(vehicles)]
     allocations = {}
     dimension = data['DIMENSION']
 
-    print 'savings length', len(savings_list)
-    print 'savings_list', savings_list
-
-    while savings_list:
+    for i, j in savings_list:
 
         if len(allocations) == dimension - 1:
             break
 
-        for i, j in savings_list:
+        demand = data['DEMAND'][i] + data['DEMAND'][j]
 
-            exausted = False
+        # Neither points are in a route
+        if i not in allocations and j not in allocations:
+            # Try to add the two nodes to a route
+            for vehicle in range(vehicles):
+                if can_add(data, [i, j], vehicles_demand[vehicle]):
+                    add_node(data, allocations, i, vehicle, vehicles_run, vehicles_demand)
+                    add_node(data, allocations, j, vehicle, vehicles_run, vehicles_demand)
+        elif (i in allocations and j not in allocations) or (j in allocations and i not in allocations): # either i or j is allocated
+            inserted = None
+            to_insert = None
 
-            print 'vehicles_run', vehicles_run
-            print 'vehicles_demand', vehicles_demand
-            print 'allocations', sorted(allocations)
-            print 'remaining', len(savings_list)
-            print 'iterating {}'.format((i, j))
-
-            demand = data['DEMAND'][i] + data['DEMAND'][j]
-
-            # Neither points are in a route
-            if i not in allocations and j not in allocations:
-                # Try to add to a route
-                fodeu_count = 0
-                for vehicle in range(vehicles):
-                    if can_add(data, [i, j], vehicles_demand[vehicle]):
-                        add_node(data, allocations, i, vehicle, vehicles_run, vehicles_demand)
-                        add_node(data, allocations, j, vehicle, vehicles_run, vehicles_demand)
-                        exausted = True
-                        break
-                    else:
-                        fodeu_count = fodeu_count + 1
-                        print 'fodeu'
-
-                if fodeu_count == vehicles:
-                    print len(savings_list)
-                    print vehicles_demand
-                    raise Exception('fodeu')
-            elif (i in allocations and j not in allocations) or (j in allocations and i not in allocations): # either i or j is allocated
-                print '{} or {} not allocated'.format(i, j)
-
-                inserted = None
-                to_insert = None
-
-                if i in allocations:
-                    inserted = i
-                    to_insert = j
-                else:
-                    inserted = j
-                    to_insert = i
-
-                if vehicles_run[allocations[inserted]].index(inserted) == 0 or vehicles_run[allocations[inserted]].index(inserted) == len(vehicles_run[allocations[inserted]]) - 1: # i not interior
-                    print '{} is not interior'.format(inserted)
-                    if can_add(data, [to_insert], vehicles_demand[allocations[inserted]]):
-
-                        append = False
-
-                        if vehicles_run[allocations[inserted]].index(inserted) == len(vehicles_run[allocations[inserted]]) - 1:
-                            append = True
-
-                        add_node(data, allocations, to_insert, allocations[inserted], vehicles_run, vehicles_demand, append)
-
-                    exausted = True
-            elif (i in allocations and j in allocations) and (allocations[i] != allocations[j]): # both allocated
-                both_not_interior = vehicles_run[allocations[i]].index(i) == 0 or vehicles_run[allocations[i]].index(i) == len(vehicles_run[allocations[i]]) - 1
-                both_interior = not both_not_interior
-
-                both_not_interior = both_not_interior and (vehicles_run[allocations[j]].index(j) == 0 or vehicles_run[allocations[j]].index(j) == len(vehicles_run[allocations[j]]) - 1)
-                both_interior = both_interior and (vehicles_run[allocations[j]].index(j) != 0 and vehicles_run[allocations[j]].index(j) != len(vehicles_run[allocations[j]]) - 1)
-
-                print 'both_not_interior? {}'.format(both_not_interior)
-
-                # if both_not_interior:
-                #     # Merge routes
-                #     if can_add(data, [j], vehicles_demand[allocations[i]]):
-                #         append = False
-
-                #         if vehicles_run[allocations[i]].index(i) == len(vehicles_run[allocations[i]]) - 1:
-                #             append = True
-
-                #         add_node(data, allocations, j, allocations[i], vehicles_run, vehicles_demand, append)
-                #         print 'merged'
-                #     elif can_add(data, [i], vehicles_demand[allocations[j]]):
-                #         append = False
-
-                #         if vehicles_run[allocations[j]].index(j) == len(vehicles_run[allocations[j]]) - 1:
-                #             append = True
-
-                #         add_node(data, allocations, i, allocations[j], vehicles_run, vehicles_demand, append)
-                #         print 'merged'
-
-                exausted = True
-
-                if both_interior:
-                    exausted = True
-                    print 'both interior'
+            if i in allocations:
+                inserted = i
+                to_insert = j
             else:
-                exausted = True
+                inserted = j
+                to_insert = i
 
+            if vehicles_run[allocations[inserted]].index(inserted) == 0 or vehicles_run[allocations[inserted]].index(inserted) == len(vehicles_run[allocations[inserted]]) - 1: # i not interior
+                if can_add(data, [to_insert], vehicles_demand[allocations[inserted]]):
+                    append = False
 
-            if exausted:
-                print 'exausted'
-                savings_list.remove((i, j))
-                break
+                    if vehicles_run[allocations[inserted]].index(inserted) == len(vehicles_run[allocations[inserted]]) - 1:
+                        append = True
 
-    print 'vehicles_run', vehicles_run
+                    add_node(data, allocations, to_insert, allocations[inserted], vehicles_run, vehicles_demand, append)
 
     return vehicles_run, savings_list
