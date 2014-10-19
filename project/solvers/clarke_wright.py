ff8aabec9e4e79fae48d1c1beb9368d75383878d
Rômulo Oliveira <romulo.ac.oliveira@gmail.com>
2014-10-19 17:13:31 -0200

    Finished
    Agora é hora de procurar os problemas

--- project/solvers/clarke_wright.py
+++ project/solvers/clarke_wright.py
@@ -3,16 +3,17 @@
 
 import operator
 
-def can_add(data, i, remaining_demand):
-    """Returns true if a vehicle with remaining demand `remaining_demand` has capacity to serve `i`"""
+def can_add(data, i, current):
+    """Returns true if a vehicle with demand `current` has capacity to serve `i`"""
     demand = data['DEMAND'][i]
+    capacity = data['CAPACITY']
 
-    if remaining_demand - demand >= 0:
+    if current + demand <= capacity:
         return True
 
     return False
 
-def add_node(data, allocation, i, vehicle, vehicles_run, vehicles_remaining_demand):
+def add_node(data, allocation, i, vehicle, vehicles_run, vehicles_demand):
     """Adds node `i` to vehicle `vehicle`
 
     Takes care of removing demand for one vehicle to another
@@ -22,22 +23,25 @@ def add_node(data, allocation, i, vehicle, vehicles_run, vehicles_remaining_dema
     #
     # Removes demand from old vehicle
     #
-
     if i in allocation:
         old_vehicle = allocation[i]
 
-        if old_vehicle:
-            vehicles_remaining_demand[old_vehicle] = vehicles_remaining_demand[old_vehicle] + demand
-            vehicles_run[old_vehicle].remove(i)
+        vehicles_demand[old_vehicle] = vehicles_demand[old_vehicle] - demand
+        vehicles_run[old_vehicle].remove(i)
 
-    vehicles_remaining_demand[vehicle] = vehicles_remaining_demand[vehicle] - demand
+        # Remove 0-nodes routes
+        if not vehicles_run[old_vehicle]:
+            del vehicles_demand[old_vehicle]
+            del vehicles_run[old_vehicle]
+
+    vehicles_demand[vehicle] = vehicles_demand[vehicle] + demand
     vehicles_run[vehicle].append(i)
     allocation[i] = vehicle
 
     return True
 
 
-def create_initial_routes(data, vehicles_run, vehicles_remaining_demand):
+def create_initial_routes(data, vehicles_run, vehicles_demand):
     """Creates a list of initial routes
 
     First, a route contains only two nodes: the depot and a node
@@ -56,19 +60,16 @@ def create_initial_routes(data, vehicles_run, vehicles_remaining_demand):
             if i > j:
                 continue
 
+            if i == j:
+                continue
+
             if i == depot:
-                added = False
+                vehicles_demand[j] = 0
+                vehicles_run[j] = []
 
-                for vehicle_index in range(len(vehicles_run)):
-                    if can_add(data, j, vehicles_remaining_demand[vehicle_index]):
-                        added = add_node(data, allocation, j, vehicle_index, vehicles_run, vehicles_remaining_demand)
-                        break
+                add_node(data, allocation, j, j, vehicles_run, vehicles_demand)
 
-                if added:
-                    routes[i][j] = 2
-                else:
-                    raise Exception('Could not allocate node {}'.format(j))
-
+                routes[i][j] = 2
             else:
                 routes[i][j] = 0
 
@@ -113,43 +114,40 @@ def solve(data, vehicles):
     """Solves the CVRP problem using Clarke and Wright Savings methods"""
     savings_list = compute_savings_list(data)
 
-    vehicles_run = [[] for _ in range(vehicles)]
-    vehicles_remaining_demand = [data['CAPACITY'] for _ in range(vehicles)]
+    vehicles_run = {}
+    vehicles_demand = {}
 
-    initial_routes, allocations = create_initial_routes(data, vehicles_run, vehicles_remaining_demand)
+    initial_routes, allocations = create_initial_routes(data, vehicles_run, vehicles_demand)
     routes = initial_routes.copy()
 
-    # print vehicles_run
-    # print vehicles_remaining_demand
+    print vehicles_run
+    print vehicles_demand
 
     depot = data['DEPOT']
 
     for i, j in savings_list:
-            #
-            # (I)   t(0,i) and t(0, j) must be greater than zero.
-            # (II)  p(i) and p(y) are not already allocated on the same truck run.
-            # (III) Removing the trucks allocated to Q(i) and Q(j) and adding a truck to cover the load Q(i)+Q(j)
-            #       does not cause the trucks allocated to exceed the trucks available
-            #
-            if routes[depot][i] > 0 and routes[depot][j] > 0:
-                if allocations[i] != allocations[j]:
-                    vehicle_a = allocations[i]
-                    vehicle_a_remaind_demand = vehicles_remaining_demand[vehicle_a]
+        #
+        # (I)   t(0,i) and t(0, j) must be greater than zero.
+        # (II)  p(i) and p(y) are not already allocated on the same truck run.
+        # (III) Removing the trucks allocated to Q(i) and Q(j) and adding a truck to cover the load Q(i)+Q(j)
+        #       does not cause the trucks allocated to exceed the trucks available
+        #
+        if routes[depot][i] > 0 and routes[depot][j] > 0:
+            if allocations[i] != allocations[j]:
+                vehicle_a = allocations[i]
+                vehicle_b = allocations[j]
 
-                    vehicle_b = allocations[j]
-                    vehicle_b_remaind_demand = vehicles_remaining_demand[vehicle_b]
-
-                    if can_add(data, j, vehicle_a_remaind_demand):
-                        add_node(data, allocations, j, vehicle_a, vehicles_run, vehicles_remaining_demand)
-                        routes[i][j] = 1
-                        routes[depot][i] = 1
-                        routes[depot][j] = 1
-                    elif can_add(data, i, vehicle_b_remaind_demand):
-                        add_node(data, allocations, i, vehicle_b, vehicles_run, vehicles_remaining_demand)
-                        routes[i][j] = 1
-                        routes[depot][i] = 1
-                        routes[depot][j] = 1
+                if can_add(data, j, vehicles_demand[vehicle_a]):
+                    add_node(data, allocations, j, vehicle_a, vehicles_run, vehicles_demand)
+                    routes[i][j] = 1
+                    routes[depot][i] = 1
+                    routes[depot][j] = 1
+                elif can_add(data, i, vehicles_demand[vehicle_b]):
+                    add_node(data, allocations, i, vehicle_b, vehicles_run, vehicles_demand)
+                    routes[i][j] = 1
+                    routes[depot][i] = 1
+                    routes[depot][j] = 1
 
     print 'vehicles_run', vehicles_run
 
-    return routes, savings_list, vehicles_run
+    return routes, savings_list, [j for i, j in vehicles_run.items()]
--- run.py
+++ run.py
@@ -27,9 +27,29 @@ def main():
     print 'ROUTING MATRIX'
     util.print_upper_triangular_matrix(routes)
 
-    print 'VEHICLES RUN'
-    print vehicles_run
+    print 'SOLUTIONS'
+    total_cost = 0
+    for solution in vehicles_run:
+        cost = util.solution_length(data, solution)
+        total_cost = total_cost + cost
+        print '{}: {}'.format(solution, cost)
+    print 'Total cost: {}'.format(total_cost)
 
+    optimum = [
+        [25, 28],
+        [27, 8, 14, 18, 20, 32, 22],
+        [7, 4, 3, 24, 29, 5, 12, 15],
+        [31, 17, 2, 13],
+        [30, 19, 9, 10, 23, 16, 11, 26, 6, 21]
+    ]
+
+    print 'OPTIMAL SOLUTIONS'
+    total_cost = 0
+    for solution in optimum:
+        cost = util.solution_length(data, solution)
+        total_cost = total_cost + cost
+        print '{}: {}'.format(solution, cost)
+    print 'Total cost: {}'.format(total_cost)
 
 if __name__ == '__main__':
     main()
