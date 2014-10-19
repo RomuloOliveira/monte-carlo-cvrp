966335c56714c4462717f570bf43fc04a13436db
Rômulo Oliveira <romulo.ac.oliveira@gmail.com>
2014-10-19 01:16:48 -0200

    Util module
    For printing and stuff

---
+++ project/util.py
@@ -0,0 +1,27 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+
+def print_upper_triangular_matrix(matrix):
+    """Prints a CVRP data dict matrix"""
+
+    # Print column header
+    print '\t',
+    for i in matrix:
+        print '{}\t'.format(i),
+    print
+
+    indent_count = 0
+
+    for i in matrix:
+        # Print line header
+        print '{}\t'.format(i),
+
+        if indent_count:
+            print '\t' * indent_count,
+
+        for j in sorted(matrix[i]): # required because dict doesn't guarantee insertion order
+            print '{}\t'.format(matrix[i][j]),
+
+        print
+
+        indent_count = indent_count + 1
