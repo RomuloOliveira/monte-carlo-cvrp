fc427b3625e149382201753bac5e1670d938777d
Rômulo Oliveira <romulo.ac.oliveira@gmail.com>
2014-10-22 12:53:34 -0200

    novo run fazendo comparação entre algoritmos

--- run.py
+++ run.py
@@ -19,12 +19,37 @@ def main():
     data = data_input.read_file(sys.argv[1])
     vehicles = int(sys.argv[2])
 
-    routes, savings_list = clarke_wright_solver.solve(data, vehicles)
+    cws_solution = clarke_wright_solver.solve(data, vehicles)
+    binary_mcscws_solution = binary_mcscws_solver.solve(data, vehicles)
 
-    print 'SAVINGS LIST MATRIX'
-    print savings_list
+    solutions = [(cws_solution, 'ClarkeWrightSolver'), (binary_mcscws_solution, 'BinaryMCSCWSSolver')]
+    valid_solutions = []
 
-    util.print_solution(routes)
+    for solution, algorithm in solutions:
+        if not solution.is_complete():
+            print 'Solution from algorithm {} not a complete solution'.format(algorithm)
+        else:
+            print '{} solution:'.format(algorithm)
+            util.print_solution(solution)
+
+            valid_solutions.append((solution, algorithm))
+
+    best_solution = None
+    best_algorithm = None
+
+    for solution, algorithm in solutions:
+        if best_algorithm is None:
+            best_algorithm = algorithm
+
+        if best_solution is None:
+            best_solution = solution
+
+        if solution.length() < best_solution.length():
+            best_solution = solution
+            best_algorithm = algorithm
+
+    print
+    print 'Best: {}'.format(best_algorithm)
 
 if __name__ == '__main__':
     main()
