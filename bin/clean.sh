0d59345dae96612128c190298bae7d7df6b2849e
Rômulo Oliveira <romulo.ac.oliveira@gmail.com>
2014-10-22 23:35:34 -0200

    opção pra limpar testes

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Uncomment to delete all previous tests
+# find -name '*.out' -path './out/*' -print0 | xargs --no-run-if-empty -0 rm -v
+
 find -name '*.pyc' -print0 | xargs --no-run-if-empty -0 rm -v
