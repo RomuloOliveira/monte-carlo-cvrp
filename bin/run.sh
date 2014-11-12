9bd921d4a6065cf6c83af4c96d0f7abfe6d23510
Rômulo Oliveira <romulo.ac.oliveira@gmail.com>
2014-11-12 16:08:29 -0200

    suportando novos names

--- bin/run.sh
+++ bin/run.sh
@@ -7,9 +7,9 @@ files=`find -name '*.vrp' | sort`
 for i in $files
 do
     basename=`echo $i | sed 's/.*\/\(.*\).vrp/\1/'`
-    vehicles=`echo $i | sed 's/.*-k\(.*\).vrp/\1/'`
+    vehicles=`echo $i | sed 's/.*-[a-zA-Z]*\([0-9]*\).*.vrp/\1/'`
     filename="out/$basename.out"
 
-    echo "Processing $basename"
+    echo "Processing $basename $vehicles"
     python run.py $i $vehicles > $filename
 done
