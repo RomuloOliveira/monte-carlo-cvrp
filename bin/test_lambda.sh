3e4a17f7ce13b5506af34a4e29cc7197483ce8df
Rômulo Oliveira <romulo.ac.oliveira@gmail.com>
2014-11-15 21:37:10 -0200

    depth first

--- bin/test_lambda.sh
+++ bin/test_lambda.sh
@@ -6,19 +6,14 @@
 . bin/build.sh
 
 instances="./input/Augerat/A-n32-k5.vrp ./input/Takes/A-n65-k9.vrp"
-for instance in $instances
+
+lambdas_p="0.0025 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0"
+for lambda_p in $lambdas_p
 do
-    if [ -f $instance ]
-    then
-        echo $instance
-    fi
-
-    basename=`echo $instance | sed 's/.*\/\(.*\).vrp/\1/'`
-    vehicles=`echo $instance | sed 's/.*-[a-zA-Z]*\([0-9]*\).*.vrp/\1/'`
-
-    lambdas_p="0.0025 0.05 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0"
-    for lambda_p in $lambdas_p
+    for instance in $instances
     do
+        basename=`echo $instance | sed 's/.*\/\(.*\).vrp/\1/'`
+        vehicles=`echo $instance | sed 's/.*-[a-zA-Z]*\([0-9]*\).*.vrp/\1/'`
         filename="out/$basename-$lambda_p.out"
         echo "Processing $basename with $vehicles vehicles and lambda_p=$lambda_p > $filename"
         python run.py $instance $vehicles > $filename
