2372c3d50cca04f347825f949ae9502f253487ef
Rômulo Oliveira <romulo.ac.oliveira@gmail.com>
2014-12-08 16:54:32 -0200

    executando 5 vezes pra cada

--- bin/run.sh
+++ bin/run.sh
@@ -10,8 +10,12 @@ for i in $files
 do
     basename=`echo $i | sed 's/.*\/\(.*\).vrp/\1/'`
     vehicles=`echo $i | sed 's/.*-[a-zA-Z]*\([0-9]*\).*.vrp/\1/'`
-    filename="out/$basename.out"
 
-    echo "Processing $basename $vehicles"
-    python run.py $i $vehicles > $filename
+    executions="1 2 3 4 5"
+    for e in $executions
+    do
+        filename="out/$basename-$e.out"
+        echo "Processing-$e $basename $vehicles"
+        python run.py $i $vehicles > $filename
+    done
 done
